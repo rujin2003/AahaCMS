19440fd085bebf49b4a13b391da1589945386a8c
rujin <rujindevkota@gmail.com>
2024-09-29 22:00:40 +0530

    Initial Commit

---
+++ AahaCMS/AahaCMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AahaCMSApp.swift
+//  AahaCMS
+//
+//  Created by Rujin on 29/09/24.
+//
+
+import SwiftUI
+
+@main
+struct AahaCMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AahaCMSTests/AahaCMSTests.swift
@@ -0,0 +1,17 @@
+//
+//  AahaCMSTests.swift
+//  AahaCMSTests
+//
+//  Created by Rujin on 29/09/24.
+//
+
+import Testing
+@testable import AahaCMS
+
+struct AahaCMSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
